30be1844a94c102a7d06f17d5d9d50cf056b3189
Joseph Cerone <jpcerone@gmail.com>
2026-01-27 21:32:12 -0500

    Initial Commit

---
+++ Blink/BlinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlinkApp.swift
+//  Blink
+//
+//  Created by Joseph Cerone on 1/27/26.
+//
+
+import SwiftUI
+
+@main
+struct BlinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
